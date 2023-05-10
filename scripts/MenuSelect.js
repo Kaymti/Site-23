32415ea13357d396cc053759d0e24500789c6848
Kevin Molinari Travezani <125293441+Kaymti@users.noreply.github.com>
2023-05-10 17:46:55 -0300

    Add files via upload

--- scripts/MenuSelect.js
+++ scripts/MenuSelect.js
@@ -1,31 +1,78 @@
-const dropdowns = document.querySelectorAll('.dropdown');
+/* Abrir e fechar ao clicar na caixa*/
+const wrapper1 = document.querySelector(".wrapper.cid1");
+const wrapper2 = document.querySelector(".wrapper.cid2");
+const wrapper3 = document.querySelector(".wrapper.tipo");
+options1 = wrapper1.querySelector(".options.cid1");
+options2 = wrapper2.querySelector(".options.cid2");
+options3 = wrapper3.querySelector(".options.tipo");
+searchInp1 = wrapper1.querySelector(".input.cid1");
+searchInp2 = wrapper1.querySelector(".input.cid2");
+searchInp3 = wrapper1.querySelector(".input.tipo");
+selectBtn1 = document.querySelector(".select-btn.cid1");
+selectBtn2 = document.querySelector(".select-btn.cid2");
+selectBtn3 = document.querySelector(".select-btn.tipo");
 
-dropdowns.forEach(dropdown => {
-    const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelectorAll('.menu li');
-    const selected = dropdown.querySelector('.selected');
-
-    select.addEventListener('click', () => {
-      select.classList.toggle('select-clicked');
-      caret.classList.toggle('caret-rotate');
-      menu.classList.toggle('menu-open');
-    });
-
-     options.forEach(option => {
-      option.addEventListener('click', () => {
-        selected.innerText = option.innerText;
-        select.classList.remove('select-clicked');
-        caret.classList.remove('caret-rotate');
-        menu.classList.remove('menu-open');
-
-        options.forEach( option => {
-            option.classList.remove('active');
-        });
-
-        option.classList.add('active');
-      });
-   });
+selectBtn1.addEventListener("click", () => {
+    wrapper1.classList.toggle("active");
 });
 
+selectBtn2.addEventListener("click", () => {
+    wrapper2.classList.toggle("active");
+});
+
+selectBtn3.addEventListener("click", () => {
+    wrapper3.classList.toggle("active");
+});
+
+/* */
+
+let Sugestao = [
+    "Caçapava", "Igaratá", "Jacareí", "Jambeiro", "Monteiro Lobato","Paraibuna", "Santa Branca", 
+    "São José dos Campos","Campos do Jordão","Lagoinha", "Natividade da Serra", "Pindamonhangaba",
+    "Redenção da Serra", "Santo Antônio do Pinhal", "São Bento do Sapucaí","São Luiz do Paraitinga",
+    "Taubaté", "Tremembé","Aparecida","Cachoeira Paulista","Canas","Cunha","Guaratinguetá","Lorena",
+    "Piquete","Potim","Roseira","Arapeí", "Areias", "Bananal","Cruzeiro","Lavrinhas","Queluz",
+    "São José do Barreiro","Silveiras","Caraguatatuba", "Ilhabela","São Sebastião","Ubatuba"
+];
+
+let tipo = [
+    "Poluicao", "Natalidade", "Mortalidade", "Criminalidade"
+];
+
+function addSugestao() {
+    Sugestao.forEach(Sugestao => {
+        let li = `<li onclick="updateNameCid(this)">${Sugestao}</li>`;
+        options1.insertAdjacentHTML("beforeend", li);
+        options2.insertAdjacentHTML("beforeend", li);
+    });
+}
+
+function addTipo() {
+    tipo.forEach(tipo => {
+        let li = `<li onclick="updateNameTipo(this)">${tipo}</li>`;
+        options3.insertAdjacentHTML("beforeend", li);
+    });
+}
+
+function updateNameCid(selectedLi){
+  if(wrapper1.classList.contains("active")){
+   wrapper1.classList.remove("active");
+   selectBtn1.firstElementChild.innerText = selectedLi.innerText;
+  }
+  else{
+    wrapper2.classList.remove("active");
+    selectBtn2.firstElementChild.innerText = selectedLi.innerText;
+  }
+}
+
+function updateNameTipo(selectedLi){
+   wrapper3.classList.remove("active");
+   selectBtn3.firstElementChild.innerText = selectedLi.innerText;
+}
+
+addSugestao();
+addTipo();
+
+searchInp1.addEventListener("onkeydown", () => {
+    console.log(searchInp1.value);
+});
--- scripts/Seletor.js
+++ scripts/Seletor.js
@@ -1,8 +1,14 @@
-let form = document.forms['my-form'];
-spliter();
+function grafico(){
+    var e1 = document.getElementById("lista1").getElementsByClassName("active");
+    var e2 = document.getElementById("lista2").getElementsByClassName("active");
+    var e3 = document.getElementById("lista3").getElementsByClassName("active");
 
-function spliter(){
-    var str = form.innerText;
-    console.log(str);
-    var res = str.split("");
+    var cid1 = e1.value;
+    var cid2 = e2.value;
+    var tipo = e3.value;
+
+    console.log(cid1);
+    console.log(cid2);
+    console.log(tipo);
+      
 }
