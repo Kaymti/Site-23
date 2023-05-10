757555aa14f25455a309f1269c94535e8e527d23
Kevin Molinari Travezani <125293441+Kaymti@users.noreply.github.com>
2023-05-10 17:47:22 -0300

    Delete Searchbar.js

--- scripts/Searchbar.js
+++
@@ -1,7 +0,0 @@
-const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("input");
-const suggbox = searchWrapper.querySelector(".autocom-box");
-
-inputBox.onkeyup = (e)=>{
-    console.log(e.target.value);
-}
