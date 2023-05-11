0c3fa674af864b00a47a808a26a89026fc2f7fb5
Kevin Molinari Travezani <125293441+Kaymti@users.noreply.github.com>
2023-05-11 19:46:32 -0300

    Add files via upload

--- scripts/MenuSelect.js
+++ scripts/MenuSelect.js
@@ -39,38 +39,60 @@ let tipo = [
     "Poluicao", "Natalidade", "Mortalidade", "Criminalidade"
 ];
 
-function addSugestao() {
+function addSugestao1(selectedCid) {
+    value1 = selectedCid;
+    options1.innerHTML = "";
     Sugestao.forEach(Sugestao => {
-        let li = `<li onclick="updateNameCid(this)">${Sugestao}</li>`;
+        let isSelected = Sugestao == selectedCid ? "selected" : "";
+        let li = `<li onclick="updateNameCid(this)" class="${isSelected}">${Sugestao}</li>`;
         options1.insertAdjacentHTML("beforeend", li);
+    });
+}
+
+function addSugestao2(selectedCid) {
+    value2 = selectedCid;
+    options2.innerHTML = "";
+    Sugestao.forEach(Sugestao => {
+        let isSelected = Sugestao == selectedCid ? "selected" : "";
+        let li = `<li onclick="updateNameCid(this)" class="${isSelected}">${Sugestao}</li>`;
         options2.insertAdjacentHTML("beforeend", li);
     });
 }
 
-function addTipo() {
+function addTipo(selectedTipo) {
+    value3 = selectedTipo;
+    options3.innerHTML =  "";
     tipo.forEach(tipo => {
-        let li = `<li onclick="updateNameTipo(this)">${tipo}</li>`;
+        let isSelected = tipo == selectedTipo ? "selected" : "";
+        let li = `<li onclick="updateNameTipo(this)" class="${isSelected}">${tipo}</li>`;
         options3.insertAdjacentHTML("beforeend", li);
     });
 }
 
 function updateNameCid(selectedLi){
   if(wrapper1.classList.contains("active")){
+   searchInp1.value = "";
+   addSugestao1(selectedLi.innerText);
    wrapper1.classList.remove("active");
    selectBtn1.firstElementChild.innerText = selectedLi.innerText;
   }
   else{
+    searchInp2.value = "";
+    addSugestao2(selectedLi.innerText);
     wrapper2.classList.remove("active");
     selectBtn2.firstElementChild.innerText = selectedLi.innerText;
   }
 }
 
 function updateNameTipo(selectedLi){
+   searchInp3.value = "";
+   addTipo(selectedLi.innerText);
    wrapper3.classList.remove("active");
    selectBtn3.firstElementChild.innerText = selectedLi.innerText;
 }
 
-addSugestao();
+addSugestao1();
+addSugestao2();
 addTipo();
 
 searchInp1.addEventListener("keyup", () => {
@@ -78,8 +100,8 @@ searchInp1.addEventListener("keyup", () => {
     let searchedVal = searchInp1.value.toLowerCase();
     arr = Sugestao.filter(data => {
         return data.toLowerCase().startsWith(searchedVal);
-    }).map(data => `<li>${data}</li>`).join("");
-    options1.innerHTML = arr;
+    }).map(data => `<li onclick="updateNameCid(this)">${data}</li>`).join("");
+    options1.innerHTML = arr ? arr : `<p>Nenhuma cidade encontrada!<p>`;
 });
 
 searchInp2.addEventListener("keyup", () => {
@@ -87,8 +109,8 @@ searchInp2.addEventListener("keyup", () => {
     let searchedVal = searchInp2.value.toLowerCase();
     arr = Sugestao.filter(data => {
         return data.toLowerCase().startsWith(searchedVal);
-    }).map(data => `<li>${data}</li>`).join("");
-    options2.innerHTML = arr;
+    }).map(data => `<li onclick="updateNameCid(this)">${data}</li>`).join("");
+    options2.innerHTML = arr ? arr : `<p>Nenhuma cidade encontrada!<p>`;
 });
 
 searchInp3.addEventListener("keyup", () => {
@@ -96,6 +118,16 @@ searchInp3.addEventListener("keyup", () => {
     let searchedVal = searchInp3.value.toLowerCase();
     arr = tipo.filter(data => {
         return data.toLowerCase().startsWith(searchedVal);
-    }).map(data => `<li>${data}</li>`).join("");
-    options3.innerHTML = arr;
+    }).map(data => `<li onclick="updateNameTipo(this)">${data}</li>`).join("");
+    options3.innerHTML = arr ? arr : `<p>Nenhum tipo encontrado!<p>`;
 });
+
+var value1;  /* cidade 1 */
+var value2;  /* cidade 2 */
+var value3;  /* tipo de dado*/
+
+function grafico(){ 
+
+  if (value1 == "Lorena")
+    console.log("azul");
+}
